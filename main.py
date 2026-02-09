ee94b32ab2ae24d31152aba2ee8465424999c43d
Kauã Diego <kauadgregorio@gmail.com>
2026-02-09 12:39:51 -0300

    Develop: Correct 1

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from flask import Flask
-
-app = Flask(__name__)
+from app import app
 
 @app.route('/')
 def homepage():
-    return "Hello, World!"
+    return "Serpa cagão!"
 
 if __name__ == '__main__':
     app.run(debug=True)
