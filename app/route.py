84323b26960b8b40e83a2a3722d9177c7623026a
Kauã Diego <kauadgregorio@gmail.com>
2026-02-16 15:16:24 -0300

    develop: teste para o serpa

--- app/route.py
+++ app/route.py
@@ -4,3 +4,7 @@ from flask import render_template
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
